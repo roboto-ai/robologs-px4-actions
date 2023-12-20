8786993871d62a0a21d3bf93861bd554361164a9
Yves <yves.albers@gmail.com>
2023-12-20 10:55:38 +0100

    Add Github Action workflow to automatically deploy Actions to roboto when pushed to dev

--- actions/px4_flight_review/scripts/setup.sh
+++ actions/px4_flight_review/scripts/setup.sh
@@ -10,6 +10,9 @@ venv_dir="$PACKAGE_ROOT/.venv"
 # Create a virtual environment
 python -m venv --upgrade-deps $venv_dir
 
+# Initialize flight review submodule
+#git submodule update --init --recursive $PACKAGE_ROOT/src/px4_flight_review/flight_review/
+
 # Install roboto
 pip_exe="$venv_dir/bin/pip"
 $pip_exe install --upgrade -r $PACKAGE_ROOT/requirements.dev.txt
