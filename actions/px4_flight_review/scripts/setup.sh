ce88c42966a1952150a1e3a1603783fab8eb9711
Yves <yves.albers@gmail.com>
2023-12-20 11:20:13 +0100

    Add submodule checkout to deploy.yml file

--- actions/px4_flight_review/scripts/setup.sh
+++ actions/px4_flight_review/scripts/setup.sh
@@ -11,7 +11,7 @@ venv_dir="$PACKAGE_ROOT/.venv"
 python -m venv --upgrade-deps $venv_dir
 
 # Initialize flight review submodule
-git submodule update --init --recursive $PACKAGE_ROOT/src/px4_flight_review/flight_review/
+#git submodule update --init --recursive $PACKAGE_ROOT/src/px4_flight_review/flight_review/
 
 # Install roboto
 pip_exe="$venv_dir/bin/pip"
